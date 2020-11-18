242f54867e07f5ef6ae09474644569fbb916f0a7
GammaGames <jesse007@ymail.com>
2020-11-17 23:01:21 -0700

    fix bg script

--- piwriter/bg.py
+++ piwriter/bg.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 
+from IT8951 import constants
 from PIL import Image
 from ttyink import TtyInk
 import os
 import random
 
 
+
+
 class BgInk(TtyInk):
     def get_screen(self):
         logo = Image.open("../img/piwriter.png")
-        logo = logo.resize((int(logo.width / 1.5), int(logo.height / 1.5)), Image.LANCZOS)
-        logo_pos = [
-            int((800 - logo.width) / 2),
-            int((600 - logo.height) / 2)
-        ]
+        # logo = logo.resize((int(logo.width / 1.5), int(logo.height / 1.5)), Image.LANCZOS)
+
         filename = random.choice(os.listdir("../img/bg"))
         image = Image.open("../img/bg/{}".format(filename))
-        target_dims = [800, 600]
-        # target_ratio = target_dims[0] / target_dims[1]
-        # image_ratio = image.width / image.height
-        # if image_ratio > target_ratio:
-        #     target_dims[1] = int(target_dims[1] * image_ratio)
-        # else:
-        #     target_dims[0] = int(target_dims[0] * image_ratio)
 
-        image = image.resize(target_dims, Image.LANCZOS)
+        image = image.resize(self.dims, Image.LANCZOS)
+        logo_pos = [
+            int((image.width - logo.width) / 2),
+            int((image.height - logo.height) / 2)
+        ]
         image.paste(logo, logo_pos, logo)
         return image
 
